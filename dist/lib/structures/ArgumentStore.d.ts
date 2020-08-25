08ef16f276933f8012d1e62ab648b5ea1b6abfd9
kyranet <contact@skyra.pw>
2020-08-25 23:44:41 +0000

    build: tsc build for 23de1b24d81fb6468f4d9662d0064abc53a08747

--- dist/lib/structures/ArgumentStore.d.ts
+++ dist/lib/structures/ArgumentStore.d.ts
@@ -1,5 +1,5 @@
 import type { Client } from 'discord.js';
-import type { Argument } from './Argument';
+import { Argument } from './Argument';
 import { BaseStore } from './base/BaseStore';
 export declare class ArgumentStore extends BaseStore<Argument> {
     constructor(client: Client);
--- dist/lib/structures/ArgumentStore.js
+++ dist/lib/structures/ArgumentStore.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ArgumentStore = void 0;
+const Argument_1 = require("./Argument");
 const BaseStore_1 = require("./base/BaseStore");
 class ArgumentStore extends BaseStore_1.BaseStore {
     constructor(client) {
         // @ts-expect-error Abstract classes are not assignable to Ctor<T>.
-        super(client, Argument);
+        super(client, Argument_1.Argument);
     }
 }
 exports.ArgumentStore = ArgumentStore;
